5e6e8937f5d6d54fa95147fa55af3a6525d67460
Anastasia Tashkenova <a_tashkenova@mail.ru>
2018-12-07 23:51:17 +0800

    Loaded files with ajax

--- views/js2.js
+++ views/js2.js
@@ -1,37 +1,55 @@
 $(document).ready(function(){
-$("#change").change(function() {
-	$("#product").html("<option disabled selected>Choose product</option>");
-	$( "#change option:selected" ).each(function() {
-      var d =  $( this ).text();
-	  $.ajax({
-		type: "POST",
-		url: "/sub",
-		data: {data:d},
-		success: function(msg){
-			str = "<option disabled selected>Choose subcategory</option>"
-			for (var i=0; i<msg.length; i++)
-				str+="<option>"+msg[i].name+"</option>"
-			$("#sub").html(str);	
-		}
-		});
-    });
+// $("#change").change(function() {
+// 	$("#product").html("<option disabled selected>Choose product</option>");
+// 	$( "#change option:selected" ).each(function() {
+//       var d =  $( this ).text();
+// 	  $.ajax({
+// 		type: "POST",
+// 		url: "/sub",
+// 		data: {data:d},
+// 		success: function(msg){
+// 			str = "<option disabled selected>Choose subcategory</option>"
+// 			for (var i=0; i<msg.length; i++)
+// 				str+="<option>"+msg[i].name+"</option>"
+// 			$("#sub").html(str);	
+// 		}
+// 		});
+//     });
+// });
+
+// $("#sub").change(function() {
+// 	$( "#sub option:selected" ).each(function() {
+//       var d =  $( this ).text();
+// 	  $.ajax({
+// 		type: "POST",
+// 		url: "/prod",
+// 		data: {data:d},
+// 		success: function(msg){
+// 			str = "<option disabled selected>Choose product</option>"
+// 			for (var i=0; i<msg.length; i++)
+// 				str+="<option>"+msg[i].name+"</option>"
+// 			$("#product").html(str);	
+// 		}
+// 		});
+//     });
+// });
+
+var data = new FormData();
+$.each(jQuery('#file')[0].files, function(i, file) {
+    data.append('file-'+i, file);
 });
 
-$("#sub").change(function() {
-	$( "#sub option:selected" ).each(function() {
-      var d =  $( this ).text();
-	  $.ajax({
-		type: "POST",
-		url: "/prod",
-		data: {data:d},
-		success: function(msg){
-			str = "<option disabled selected>Choose product</option>"
-			for (var i=0; i<msg.length; i++)
-				str+="<option>"+msg[i].name+"</option>"
-			$("#product").html(str);	
-		}
-		});
-    });
+$.ajax({
+    url: '/load',
+    data: data,
+    cache: false,
+    contentType: false,
+    processData: false,
+    method: 'POST',
+    type: 'POST', 
+    success: function(data){
+        console.log(data);
+    }
 });
 
 });
--- views/js_file.js
+++ views/js_file.js
@@ -1,4 +1,32 @@
 $(document).ready(function(){
+
+$("#fup").submit(function() {
+	
+	$(this).ajaxSubmit({
+
+		error: function(xhr) {
+        status('Error: ' + xhr.status);
+            },
+		success: function(spec){
+		        var isAllowed = confirm("Вы уверены, что хотите добавить (перезаписать) "+
+		        	"учебный план '"+spec+" года'?")
+		        if (isAllowed) {
+		        	$.ajax({
+					type: "POST",
+					url: "/sub",
+					success: function(msg){
+						alert(msg)	
+					}
+					});
+		        }
+
+		        }
+		    
+	});
+	return false;
+	});
+});
+
 /*$("#change").change(function() {
 	$("#product").html("<option disabled selected>Choose product</option>");
 	$( "#change option:selected" ).each(function() {
@@ -17,13 +45,8 @@ $(document).ready(function(){
     });
 });*/
 
-$("#sub").click(function() {
-	  $.ajax({
-		type: "POST",
-		url: "/sub",
-		success: function(msg){
-			alert(msg);	
-		}
-		});
-    });
-});
+
+	
+
+
+
